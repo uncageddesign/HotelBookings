b422b0aaab2eee734d59536b0ad37b7ef1e228b2
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2019-09-04 13:54:15 +0100

    basic interface completed

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
+    <h1>Hotel Bookings</h1>
     <booking-form />
+    <guest-list />
   </div>
 </template>
 
 <script>
-
+import BookingForm from '@/components/BookingForm';
+import GuestList from '@/components/GuestList';
 
 export default {
   name: 'app',
   components: {
-    'booking-form': BookingForm
+    'booking-form': BookingForm,
+    'guest-list': GuestList
   }
 }
 </script>
--- client/src/services/BookingServices.js
+++
@@ -1,8 +0,0 @@
-const baseURL = 'http://localhost:3000/api/bookings/'
-
-export default {
-  getBooking(){
-    return fetch(baseURL)
-    .then(res => res.json())
-  }
-}
